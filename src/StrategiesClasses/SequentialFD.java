c474eb40c36c4d937b6bc72ca05625dfa6255999
Adrian-OCS <adrian.cruz4@upr.edu>
2022-04-19 14:20:49 -0400

    Implemented sequential count strategy

--- src/StrategiesClasses/SequentialFD.java
+++ src/StrategiesClasses/SequentialFD.java
@@ -2,12 +2,14 @@ package StrategiesClasses;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * This class corresponds to the Sequential strategy to count frequencies in an
  * array list.
- * @author Fernando J. Bermudez && YOUR NAME HERE
+ * @author Fernando J. Bermudez && Adrián O. Cruz Silva
  *
  * @param <E> The type of the elements whose frequencies are being counted.
  */
@@ -27,7 +29,30 @@ public class SequentialFD<E extends Comparable<E>> extends AbstractFDStrategy<E>
 	@Override
 	public ArrayList<Map.Entry<E, Integer>> computeFDList(ArrayList<E> dataSet) {
 		/*TODO ADD YOUR CODE HERE*/
-		return null; //Dummy Return
+		
+		ArrayList<Map.Entry<E, Integer>> results = new ArrayList<Map.Entry<E, Integer>>();
+		
+		results.add(new AbstractMap.SimpleEntry<E, Integer>(dataSet.get(0), 1));
+		
+		for (int i=1; i<dataSet.size(); i++) {
+			
+			boolean found = false;
+			
+			for (int j=0; j<results.size(); j++) {
+				if (results.get(j).getKey().equals(dataSet.get(i))) {
+					results.get(j).setValue(results.get(j).getValue() + 1);
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found) {
+				results.add(new AbstractMap.SimpleEntry<E, Integer>(dataSet.get(i), 1));
+			}
+							
+		} 
+
+		return results; 
 	}
 
 }
