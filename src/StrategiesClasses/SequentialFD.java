5c9d373d86f41295547ff1b73a86589eb36a4f6a
Adrian-OCS <adrian.cruz4@upr.edu>
2022-04-19 14:36:47 -0400

    Commented sequential strategy

--- src/StrategiesClasses/SequentialFD.java
+++ src/StrategiesClasses/SequentialFD.java
@@ -28,16 +28,20 @@ public class SequentialFD<E extends Comparable<E>> extends AbstractFDStrategy<E>
 	 */
 	@Override
 	public ArrayList<Map.Entry<E, Integer>> computeFDList(ArrayList<E> dataSet) {
-		/*TODO ADD YOUR CODE HERE*/
 		
+		//Creates resulting array list of map entries
 		ArrayList<Map.Entry<E, Integer>> results = new ArrayList<Map.Entry<E, Integer>>();
 		
+		//Adds dataSet's 1st entry to results, in order to make looping and comparisons possible
 		results.add(new AbstractMap.SimpleEntry<E, Integer>(dataSet.get(0), 1));
 		
+		//Loops through all dataSets elements
 		for (int i=1; i<dataSet.size(); i++) {
 			
 			boolean found = false;
 			
+			//Loops through all "j" elements to compare their keys to the current dataSet element i.
+			//If condition met (current dataSet i object is already in results), counter value++ and loop breaks
 			for (int j=0; j<results.size(); j++) {
 				if (results.get(j).getKey().equals(dataSet.get(i))) {
 					results.get(j).setValue(results.get(j).getValue() + 1);
@@ -46,6 +50,7 @@ public class SequentialFD<E extends Comparable<E>> extends AbstractFDStrategy<E>
 				}
 			}
 			
+			//If dataSet i not in results, it gets added as key of a new map entry with value 1
 			if (!found) {
 				results.add(new AbstractMap.SimpleEntry<E, Integer>(dataSet.get(i), 1));
 			}
