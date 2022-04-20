60ccdd1ebb1d85213d2fde83bc72f308a69acb9f
Adrian-OCS <adrian.cruz4@upr.edu>
2022-04-20 18:13:08 -0400

    Implemented Map Strategy

--- src/StrategiesClasses/MapFD.java
+++ src/StrategiesClasses/MapFD.java
@@ -1,6 +1,7 @@
 package StrategiesClasses;
 
 import java.util.ArrayList;
+
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 /**
  * This class implements the Map/Hash table strategy to count frequencies in an ArrayList.
- * @author Fernando J. Bermudez && YOUR NAME HERE
+ * @author Fernando J. Bermudez && Adrián O. Cruz Silva
  *
  * @param <E> The type of the elements whose frequencies are being counted.
  */
@@ -28,8 +29,31 @@ public class MapFD<E extends Comparable<E>> extends AbstractFDStrategy<E> {
 	 */
 	@Override
 	public ArrayList<Entry<E, Integer>> computeFDList(ArrayList<E> dataSet) {
-		/*TODO ADD YOUR CODE HERE*/
-		return null; //Dummy Return
+		
+		//Creates a hashtable from which entries will be copied to a final results array list
+		Hashtable<E, Integer> table = new Hashtable<E, Integer>();
+		ArrayList<Map.Entry<E, Integer>> results = new ArrayList<Map.Entry<E, Integer>>();
+		
+		//Loops through dataSet elements. 
+		//If iteration key not in the hashtable, adds it with value 1
+		//Else, increase the found key's value by 1 
+		for (int i=0; i<dataSet.size(); i++) {
+			if (table.containsKey(dataSet.get(i))) {
+				table.put(dataSet.get(i), table.get(dataSet.get(i))+1);
+			}
+			else {
+				table.put(dataSet.get(i), 1);
+			}
+		}
+		
+		//Set will copy hashtable elements to be able to loop through them and add them to results
+		Set<Map.Entry<E, Integer>> tableKeys = table.entrySet(); 
+		
+		for (Entry<E, Integer> i: tableKeys) {
+			results.add((Entry<E, Integer>) i);
+		}
+		
+		return results; 
 	}
 
 }
